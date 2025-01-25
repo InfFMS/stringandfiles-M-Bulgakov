962910c310a16dbd0b2c8e478cc7e5c614c63418
Булгаков Михаил Сергеевич <m.bulgakov@main.nsu.ru>
2025-01-25 13:46:33 +0700

    Сделал 1

--- task1.py
+++ task1.py
@@ -5,15 +5,19 @@
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
 import re
-f = open('task2.txt', encoding="utf-8")
-s1 = f.read()
+f = open('task1.txt', encoding="utf-8")
+e = f.read()
+s1 = e
 s2 = s1.split('\n')
 print(len(s2))
 s = re.split(r'[.—\n,! ?-]+',s1)
+s = s[:-1:]
 print(len(s))
-s = re.split(r'[.—\n,!?-]+',s1)
-s1 = ''.join(s)
-print(len(s1))
+
+a = e
+s = re.split(r'[.—]+',a)
+a = ''.join(s)
+print(len(a))
 
 f.close()
 
